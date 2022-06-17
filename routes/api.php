2f73a4b14f5207f46491ea7c7a3c722372a3131b
Marcos-AlonsoCon <marcos.alonso@cceo.com.mx>
2022-06-17 16:05:35 -0500

    Initial commit (Laravel Sanctum): :tada: I start section 14 of the Laravel course

--- routes/api.php
+++ routes/api.php
@@ -20,6 +20,11 @@ Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
 });
 
+// PROTECTING API ROUTES WITH SANCTUM
+Route::group(['middleware' => 'auth:sanctum'], function(){
+    Route::resource('category', CategoryController::class)->except(["create","edit"]);
+    Route::resource('post', PostController::class)->except(["create","edit"]);
+});
 
 Route::get('post/all', [PostController::class, 'all']);
 Route::get('post/slug/{post:slug}', [PostController::class, 'slug']);
@@ -27,5 +32,5 @@ Route::get('category/all', [CategoryController::class, 'all']);
 Route::get('category/slug/{post}', [CategoryController::class, 'slug']);
 Route::get('category/{category}/posts', [CategoryController::class, 'posts']);
 // DELCARES THE CATEGORY API ROUTES EXCEPT create AND edit WICH ARE NEVER USED
-Route::resource('category', CategoryController::class)->except(["create","edit"]);
-Route::resource('post', PostController::class)->except(["create","edit"]);
+// Route::resource('category', CategoryController::class)->except(["create","edit"]);
+// Route::resource('post', PostController::class)->except(["create","edit"]);
